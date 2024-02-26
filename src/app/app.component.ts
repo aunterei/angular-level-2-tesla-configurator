395ba735d511670e1f3c05ce91510a26d235a711
Antoine Untereiner <untereiner.a@sfeir.com>
2024-02-26 12:39:00 +0100

    Move car image to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@core';
+import { ImageContainerComponent } from '@shared';
+import { CarConfiguratorService } from '@features/configurator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, RouterOutlet, NavbarComponent],
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    RouterOutlet,
+    NavbarComponent,
+    ImageContainerComponent,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected configuratorService: CarConfiguratorService = inject(
+    CarConfiguratorService,
+  );
+}
