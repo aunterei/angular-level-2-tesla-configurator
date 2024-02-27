6fec87bc4bd23dd1ef702a010f12ae3eab85faa2
Antoine Untereiner <untereiner.a@sfeir.com>
2024-02-27 09:38:55 +0100

    Fix unit tests

--- src/app/shared/components/select/select.component.spec.ts
+++ src/app/shared/components/select/select.component.spec.ts
@@ -3,16 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectComponent } from './select.component';
 
 describe('SelectComponent', () => {
-  let component: SelectComponent;
-  let fixture: ComponentFixture<SelectComponent>;
+  let component: SelectComponent<string>;
+  let fixture: ComponentFixture<SelectComponent<string>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SelectComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SelectComponent);
+      imports: [SelectComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SelectComponent<string>);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
